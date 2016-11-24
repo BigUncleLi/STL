0d0ab1fe98a9762afc61e343df642ded758fc70d
BigUncleLi <BigUncleLi@163.com>
2016-11-24 16:20:08 +0800

    style : add some notes for bubble sort
    hi'll decrease every time on outer layout. <--
    lo'll increase every time on inner layout. -->
    
    record the tag to jduge whether vector is ordered

--- Section-1/vector/main.cpp
+++ Section-1/vector/main.cpp
@@ -5,36 +5,48 @@
 #include "vector_implement.h"
 
 int main(){
+	// test vector constructor
 	Vector<int> v(3, 2, 1);
+
+	// test vector [] operator
 	cout << "v[1] : " << v[1] << endl;
+
+	// test vector insert(r, e) | insert(e)
 	v.toString();
 	v.insert(2, 6);
 	v.insert(2);
 	v.insert(-3);
 	v.toString();
 
+	// test disordered vector find
 	cout << "result of finding 1 is : " << v.find(1) << endl;
 	cout << "result of finding -1 is : " << v.find(-1) << endl;
 
+	// test disordered vector sort
 	v.sort();
 	cout << "result of sort is : " << endl;
 	v.toString();
 
+	// test disordered vector remove 
 	v.remove(2);
 	cout << "result of remove index of 2 is : " << endl;
 	v.toString();
 
+	// test disordered vector deduplicate
 	v.deduplicate();
 	cout << "after deduplicate : " << endl;
 	v.toString();
 
+	// test ordered vector insert(e)
 	Vector<int> orderedVector(3, 3, 1);
 	orderedVector.insert(2);
 	orderedVector.insert(3);
 	orderedVector.toString();
 
+	// test ordered vector search
 	cout << "ordered vector search 2 is : " << orderedVector.search(2) << endl;
 
+	// test ordered vector uniquify
 	orderedVector.uniquify();
 	cout << "ordered vector after uniquify : "
 	orderedVector.toString();
--- Section-1/vector/vector_sort_bubble.h
+++ Section-1/vector/vector_sort_bubble.h
@@ -3,13 +3,18 @@
 #include "vector.h"
 
 template <typename T> void Vector<T>::bubbleSort(Rank lo, Rank hi){
+	// the result of bubble function is sorted
+	// if vector is not sorted, try bubble again.
+	// scale'll decrease by hi every time ( <-- )
 	while (!bubble(lo, hi--));
 }
 
 template <typename T> bool Vector<T>::bubble(Rank lo, Rank hi) {
 	bool sorted = true;
+	// scale'll decrease by lo every time ( --> )
 	while (++lo < hi) {
 		if (_elem[lo - 1] > _elem[lo]) {
+			// if reversed pair is existing, take tag to false
 			sorted = false;
 			swap(_elem[lo - 1], _elem[lo]);
 		}
