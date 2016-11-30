6bf5fcdf3100d9e1478df40206b77970d044c8b7
BigUncleLi <BigUncleLi@163.com>
2016-11-30 11:27:33 +0800

    feat : add list node insertAs pred/succ
    insertAsPred :
      ListNodePosi(T) x = new ListNode(e, pred, this);
      pred->succ = x;
      pred = x;
    insertAsSucc :
      ListNodePosi(T) x = new ListNode(e, this, succ);
      succ->pred = x;
      succ = x;

--- Section-1/list/listnode_impelment.h
+++ Section-1/list/listnode_impelment.h
@@ -2,12 +2,23 @@
 #include "listnode.h"
 
 template <typename T> ListNodePosi(T) ListNode<T>::insertAsPred(T const & e) {
-	//new ListNode x. pred <- x -> this
+	// new ListNode x. pred <- x -> this
 	ListNodePosi(T) x = new ListNode(e, pred, this);
 	// Notice : step2 cannot run before step1. (or it'll override pred)
 	// step 1 : pred -> x
 	pred->succ = x;
 	// step 2 : x <- this
 	pred = x;
+	return x;
+}
+
+template <typename T> ListNodePosi(T) ListNode<T>::insertAsSucc(T const & e) {
+	// new ListNode x. this <- x -> succ
+	ListNodePosi(T) x = new ListNode(e, this, succ);
+	// Notice : step2 cannot run before step1. (or it'll override succ)
+	// step 1 : x <- succ
+	succ->pred = x;
+	// step 2 : this -> x
+	succ = x;
 	return x;
 }
